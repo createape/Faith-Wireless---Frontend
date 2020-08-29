fa8b1c946357d443d83e29a4b721ec7f45a44e29
Rafael Mancebo <rmancebo@createape.com>
2020-08-28 20:47:33 -0400

    Admin Panel - Profile Done

--- Admin Panel/js/app.js
+++ Admin Panel/js/app.js
@@ -11,8 +11,8 @@ $(function () {
   }).trigger("resize");
 
 
-  if($('.data-table').length > 0){
-      //  Data tables
+  if ($('.data-table').length > 0) {
+    //  Data tables
     var table = $('.data-table').DataTable();
 
     $('#search-input').on('keyup', function () {
@@ -52,28 +52,77 @@ $(function () {
   }
 
 
-//  Form controls
-$(".form-group").on("change",function () {
+  //  Form controls
+  $(".form-group").on("change", function () {
 
-  if($(this).find(".form-control").get(0).checkValidity()){
-    $($(this).find(".form-control").get(0)).parent().addClass("is-valid");
-    $($(this).find(".form-control").get(0)).parent().removeClass("is-invalid ");
-  }else{
-    $($(this).find(".form-control").get(0)).parent().addClass("is-invalid ");
-    $($(this).find(".form-control").get(0)).parent().removeClass("is-valid");
+    if ($(this).find(".form-control").get(0).checkValidity()) {
+      $($(this).find(".form-control").get(0)).parent().addClass("is-valid");
+      $($(this).find(".form-control").get(0)).parent().removeClass("is-invalid ");
+    } else {
+      $($(this).find(".form-control").get(0)).parent().addClass("is-invalid ");
+      $($(this).find(".form-control").get(0)).parent().removeClass("is-valid");
+    }
+
+  });
+
+  $(".form-group").on("keyup", function () {
+    $(".form-group .form-control").each(function () {
+      if ($(this).val().length > 0) {
+        $(this).parent().addClass("has-content");
+      } else {
+        $(this).parent().removeClass("has-content");
+      }
+    });
+  });
+
+  // Slider
+  $('.info-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true
+  });
+  //  Charts
+
+  if ($(".chart-content-item").length > 0) {
+    Chart.defaults.global.legend.display = false;
+    Chart.defaults.global.tooltips.enabled = false;
+    var charts = [];
+    $(".chart-content-item").each(function () {
+
+      var type = $(this).data("type");
+      // var labels = $(this).data("labels").split(",");
+      var data = $(this).data("data").split(",");
+      var colors = $(this).data("colors").split(",");
+      for (i = 0; i < data.length; i++) {
+        data[i] = parseFloat(data[i]);
+      }
+
+      var chart = new Chart(this.getContext('2d'), {
+        // The type of chart we want to create
+        type: type,
+        // The data for our dataset
+        data: {
+          // labels: labels,
+          datasets: [{
+            "backgroundColor": colors,
+            data: data,
+            borderWidth: 0,
+
+          }]
+        },
+
+        // Configuration options go here
+        options: {
+          responsive: true,
+        }
+      });
+      // chart.defaults.global.legend.display(false);
+      charts.push(chart);
+    });
   }
 
-});
-
-$(".form-group").on("keyup",function () {
-  $(".form-group .form-control").each(function () {
-    if($(this).val().length > 0){
-      $(this).parent().addClass("has-content");
-    }else{
-      $(this).parent().removeClass("has-content");
-    }
-  });
-});
 
 
 });
